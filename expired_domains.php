63abd3f87252f6b8ea33475b7eccb05cd1413110
Ertuğrul TURAN <60829297+obirninja@users.noreply.github.com>
2020-04-12 07:06:48 +0300

    Add files via upload

---
+++ expired_domains.php
@@ -0,0 +1,53 @@
+﻿<!-- NinjaNetwork / Obir.Ninja -->
+<?php
+// Edit (add your expireddomains.net account cookie)
+$cookiereme = "T13Rv55%3A%7C%3AhRZzTcAef2JM693XJGBzKdxhcdPkWdrLLPMSrduH";
+$cookieExpiredDomainssessid = "te1th052f3i25gadgbd7o7vt42";
+// Edit (add your expireddomains.net account cookie)
+$uzantisec = $_GET["uzanti"];
+sleep(2);
+//coded by v4r1able & t13r
+if(isset($_GET["u"])) {
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://member.expireddomains.net".$_GET["u"]."");
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_COOKIE, "reme=".$cookiereme.";");
+curl_setopt($curl, CURLOPT_COOKIE, "ExpiredDomainssessid=".$cookieExpiredDomainssessid.";");
+$girlo = curl_exec($curl);
+preg_match_all('@data-clipboard-text="(.*?)"@si',$girlo,$cek);
+preg_match_all('@<a rel="nofollow" class="next" href="(.*?)">Next Page &#187;</a>@si',$girlo,$cek_);
+echo $cek[1][0];
+$v4_t1 = fopen('v4_t1.txt', 'a+');
+fwrite($v4_t1, $cek[1][0]);
+fclose($v4_t1);
+echo '<br><br>';
+echo '<meta http-equiv="refresh" content="30;URL=expired_domains.php?uzanti='.$uzantisec.'&u='.$cek_[1][0].'">';
+echo '<meta http-equiv="refresh" content="30;URL=expired_domains.php?uzanti='.$uzantisec.'&u='.$cek_[1][0].'">';
+echo '<meta http-equiv="refresh" content="30;URL=expired_domains.php?uzanti='.$uzantisec.'&u='.$cek_[1][0].'">';
+curl_close($curl);
+exit;
+}
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://member.expireddomains.net/domains/".$uzantisec."/?flimit=300");
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_COOKIE, "reme=".$cookiereme.";");
+curl_setopt($curl, CURLOPT_COOKIE, "ExpiredDomainssessid=".$cookieExpiredDomainssessid.";");
+$girlo = curl_exec($curl);
+preg_match_all('@data-clipboard-text="(.*?)"@si',$girlo,$cek);
+preg_match_all('@<a rel="nofollow" class="next" href="(.*?)">Next Page &#187;</a>@si',$girlo,$cek_);
+echo $cek[1][0];
+$v4_t1 = fopen('v4_t1.txt', 'a+');
+fwrite($v4_t1, $cek[1][0]);
+fclose($v4_t1);
+echo '<br><br>';
+echo '<meta http-equiv="refresh" content="30;URL=expired_domains.php?uzanti='.$uzantisec.'&u='.$cek_[1][0].'">';
+echo '<meta http-equiv="refresh" content="30;URL=expired_domains.php?uzanti='.$uzantisec.'&u='.$cek_[1][0].'">';
+echo '<meta http-equiv="refresh" content="30;URL=expired_domains.php?uzanti='.$uzantisec.'&u='.$cek_[1][0].'">';
+curl_close($curl);
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<title>expired_domains_bot</title><br><br>
+<body>
+<center>
+<img title="V4T1 PROJECT" src="https://cdn.r10.net/editor/115364/2119976671.png"></br>
+<form action="expired_domains.php" method="get">
+Uzantı: <select name="uzanti">
+  <option value="combinedexpired">Karışık</option>
+  <option value="expiredcom">.com</option>
+  <option value="expirednet">.net</option>
+  <option value="expiredbiz">.biz</option>
+  <option value="expiredinfo">.info</option>
+  <option value="expiredorg">.org</option>
+  <option value="expiredac">.ac</option>
+  <option value="expiredio">.io</option>
+  <option value="expiredpw">.pw</option>
+  <option value="expiredeu">.eu</option>
+  <option value="expiredclub">.club</option>
+  <option value="expiredcloud">.cloud</option>
+  <option value="expiredonline">.online</option>
+  <option value="expiredxyz">.xyz</option>
+  <option value="pendingdelete">Pending delete</option>
+</select> 
+<input value="Dump Domains" type="submit">
+</form>
+</center>
+</body>
+</html> 
